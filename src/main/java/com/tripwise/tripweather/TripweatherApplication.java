3d6276cc17ac94ff7bb5415dc71189bf564a2f28
Ornella <ornelladelvicario@gmail.com>
2025-08-20 12:31:23 +0200

    feat: Initial project setup with Spring Initializr

---
+++ src/main/java/com/tripwise/tripweather/TripweatherApplication.java
@@ -0,0 +1,13 @@
+package com.tripwise.tripweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripweatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripweatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripwise/tripweather/TripweatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripwise.tripweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripweatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
